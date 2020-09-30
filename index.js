71bc49497e2435b26287cc231ef7e5705eeb0372
Vinicius Branco <vinicius.branco@dbserver.com.br>
2020-09-30 20:30:45 -0300

    feat: swagger setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const http = require('http');
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const routes = require('./routes.js');
 const database = require("./services/database");
 const cors = require("cors")({
@@ -15,6 +18,12 @@ const server = {
     http: http.createServer(app),
 };
 
+var options = {
+    explorer: true
+};
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 
@@ -25,5 +34,5 @@ console.log("Starting Server")
 
 // Start server
 server.http.listen(process.env.HTTP_PORT, () => {
-    console.log(`Listening on http://localhost:${ process.env.HTTP_PORT }`);
+    console.log(`Listening on http://localhost:${process.env.HTTP_PORT}`);
 });
